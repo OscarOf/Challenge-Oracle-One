3e38deabbdbdf5812ffdb6484c508fb16a3aa285
Óscar O <oscarforjuela@hotmail.com>
2023-04-22 13:05:13 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/*La letra "a" es convertida para "ai"
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+//const text = 'felicidades por enfrentar este desafio y haberlo concluido con exito!';
+
+//console.log(text.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat"));
+
+//let a = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!";        
+
+//console.log(a.replaceAll("ai", "a").replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ober", "o").replaceAll("ufat", "u"));
+
+var textEncrip = document.getElementById("textoE");
+var botonEncrip = document.getElementById("botonE");
+
+function encriptar() {        
+    //console.log(textEncrip.value);
+    if(textEncrip.value == ""){
+        document.write("escribe algo");
+    }else{
+        let res = textEncrip.value.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat");
+        document.write(res);
+    }
+}
+
+botonEncrip.onclick = encriptar;
